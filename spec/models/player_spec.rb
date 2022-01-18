abe1c4d2ccea9a609db5d3222b40c6af5aae8919
Parker Lockhartn <parkerlockhart42@gmail.comm>
2022-01-18 10:57:51 -0600

    Add competition relationship test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  it { should belong_to :team }
+  describe 'relationships' do
+    it { should belong_to :team }
+    it { have_many(:competitions).through(:team) }
+  end
 end
