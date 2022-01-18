39a5dc9cecb687cf12e06b79cdb6cf4959e079be
Parker Lockhartn <parkerlockhart42@gmail.comm>
2022-01-18 10:58:48 -0600

    Add player association and player_avg_age instance method

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,9 @@
 class Competition < ApplicationRecord
   has_many :competition_teams
   has_many :teams, through: :competition_teams
-end 
+  has_many :players, through: :teams
+
+  def players_avg_age
+    players.average(:age)
+  end 
+end
