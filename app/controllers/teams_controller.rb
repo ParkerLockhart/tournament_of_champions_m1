52c5cedd02514ed7f7e0165c729cb33f468183b2
Parker Lockhartn <parkerlockhart42@gmail.comm>
2022-01-18 11:39:48 -0600

    Add team create action to controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,22 @@
 class TeamsController < ApplicationController
   def new
+    @competition = Competition.find(params[:competition_id])
+  end
+
+  def create
+    @competition = Competition.find(params[:competition_id])
+    team = @competition.teams.create(team_params)
+    if team.save
+      redirect_to competition_path(@competition)
+    else
+      redirect_to "/competitions/#{@competition.id}/teams/new"
+      flash[:alert] = "Error: unable to register team, please try again"
+    end
+  end
+
+private
+
+  def team_params
+    params.require(:team).permit(:nickname, :hometown, :competition_id)
   end
 end
